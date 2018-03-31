f74d761bde1cbf34d7781d6694f01d6e15d0cea8
Paul <gansca.paul@gmail.com>
2018-04-01 00:37:33 +0100

    Initial commit

---
+++ app/src/main/java/com/ganscapaul/stormy/AlertDialogFragment.java
@@ -0,0 +1,22 @@
+package com.ganscapaul.stormy;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.os.Bundle;
+
+public class AlertDialogFragment extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Context context = getActivity();
+        AlertDialog.Builder builder = new AlertDialog.Builder(context)
+                .setTitle(context.getString(R.string.error_title))
+                .setMessage(context.getString(R.string.error_message))
+                .setPositiveButton(R.string.error_ok_button_text, null);
+
+        AlertDialog dialog = builder.create();
+        return dialog;
+    }
+}
---
+++ app/src/main/java/com/ganscapaul/stormy/MainActivity.java
@@ -0,0 +1,83 @@
+package com.ganscapaul.stormy;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String apiKey = "9cd787e66f31e55681fff3a67a873d47";
+        double latitude = 37.8267;
+        double longitude = -122.4233;
+        String forecastUrl = "https://api.darksky.net/forecast/" + apiKey +
+                "/" + latitude + "," + longitude;
+
+        if (isNetworkAvailable()) {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(forecastUrl)
+                    .build();
+
+            Call call = client.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    try {
+                        Log.v(TAG, response.body().string());
+                        if (response.isSuccessful()) {
+                        } else {
+                            alertUserAboutError();
+                        }
+                    } catch (IOException e) {
+                        Log.e(TAG, "Exception Caught: ", e);
+                    }
+                }
+            });
+        } else {
+            AlertDialogFragment dialog = new AlertDialogFragment();
+            dialog.show(getFragmentManager(), "error_dialog");
+        }
+
+        Log.d(TAG, "Main UI code running!");
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager manager = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        boolean isAvailable = false;
+        if (networkInfo != null && networkInfo.isConnected()){
+            isAvailable = true;
+        }
+        return isAvailable;
+    }
+
+    private void alertUserAboutError() {
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        dialog.show(getFragmentManager(), "error_dialog");
+    }
+}
